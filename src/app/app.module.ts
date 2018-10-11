a4cb013f84f3ee16fe69812b6cf00b1394e25cd9
Valentin Coudert <vallenain@free.fr>
2018-10-11 20:19:01 +0200

    added angular material + started front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
+
