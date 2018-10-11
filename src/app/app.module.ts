5ae98adc0f98ba64dc823beef1ae77200f6724c6
Valentin Coudert <vallenain@free.fr>
2018-10-11 23:25:34 +0200

    added header + router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { registerLocaleData } from "@angular/common";
+import localeFr from '@angular/common/locales/fr';
 
+import { environment } from './../environments/environment';
 import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { MapDashboardComponent } from './map-dashboard/map-dashboard.component';
+import { DataDashboardComponent } from './data-dashboard/data-dashboard.component';
 
+registerLocaleData(localeFr, 'fr');
+
+const appRoutes: Routes = [
+  {
+    path: 'map',
+    component: MapDashboardComponent
+  },
+  {
+    path: 'data',
+    component: DataDashboardComponent
+  },
+  {
+    path: 'admin',
+    component: AdminDashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/map',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AdminDashboardComponent,
+    MapDashboardComponent,
+    DataDashboardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: !environment.production }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
